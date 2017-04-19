9a55bc8ac49608a4549ce9f684ac53f594105704
MYHITGITBIT <1120310228@hit.edu.cn>
2017-04-19 14:27:25 +0800

    Ac No 88

--- 88-Merge-Sorted-Array.py
+++ 88-Merge-Sorted-Array.py
@@ -1,29 +1,14 @@
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
-        """
-        :type nums1: List[int]
-        :type m: int
-        :type nums2: List[int]
-        :type n: int
-        :rtype: void Do not return anything, modify nums1 in-place instead.
-        """
-        i = 0; j = 0; k = 0;
-        while i < m and j < n :
-            if nums1[i] < nums2[j]:
-            
-        '''
-        class Solution {
-public:
-    void merge(int nums1[], int m, int nums2[], int n) {
-        int ia = m - 1, ib = n - 1, icur = m + n - 1;
-        while(ia >= 0 && ib >= 0)
-            nums1[icur--] = nums1[ia] > nums2[ib] ? nums1[ia--] : nums2[ib--];
-        while(ib >=0)
-            num1[icur--] = num1[ib--];
-    }
-};
-        '''
-if __name__ == '__main__':
-    s = Solution()
-    print (s.merge([0], 0, [1], 1))
-    pass
+        nums1 = nums1 + [0]*n
+        while m > 0 and n > 0:
+            if nums1[m-1] >= nums2[n-1]:
+                nums1[m+n-1] = nums1[m-1]
+                m -= 1
+            else:
+                nums1[m+n-1] = nums2[n-1]
+                n -= 1
+        print(nums1)
+        
+        if n > 0:
+            nums1[:n] = nums2[:n]
