b32778023ffaa19d2d0eb8f73587be73147e0e9f
MYHITGITBIT <1120310228@hit.edu.cn>
2017-04-21 23:03:44 +0800

    Ac 204 with optimization

--- 204-countPrime-easy.py
+++ 204-countPrime-easy.py
@@ -1,6 +1,6 @@
-from math import sqrt
+import time
 class Solution(object):
-    def countPrimes(self, n):
+    def countPrimes1(self, n):
         """
         :type n: int
         :rtype: int
@@ -8,8 +8,6 @@ class Solution(object):
         ans = 0
         if n == 0 or n == 1 or n == 2:
             return 0
-        
-
         
         for i in range(2, n):
             if i % 2 == 0:
@@ -20,19 +18,26 @@ class Solution(object):
 
         return ans + 1
         
-    def isPrimes(self, n):
+    def isPrimes1(self, n):
         if n == 1:
             return False
         
         if n % 2 == 0:
             return False
         
-        for i in range(2, int(sqrt(n))+1):
+        for i in range(2, int(n**0.5) +1):
             if n % i == 0:
                 return False
         return True
-'''
-if __name__ == '__main__':
-    s = Solution()
-    print (s.countPrimes(100))
-'''
+    
+    def countPrimes(self, n):
+        s = [True] * n
+        s[0] = s[1] = False
+        sqrtn = int(round(n ** 0.5))
+        count = 0
+        for i in range(2, sqrtn + 1):  # xrange for Py2
+            if s[i]:
+                count += 1
+                s[i * i: n: i] = [False] * len(range(i * i, n, i))  # xrange for Py2
+        print(count)
+        return s.count(True)
