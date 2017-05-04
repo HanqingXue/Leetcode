dda9d46a0e9921ca7927817621ff43c230d19a0f
MYHITGITBIT <1120310228@hit.edu.cn>
2017-05-04 15:04:45 +0800

    Generate samples

--- algorithm/SCP/scp.py
+++ algorithm/SCP/scp.py
@@ -1,18 +1,49 @@
 import pulp
+import random
 
 class SCP(object):
     def __init__(self):
         super(SCP, self).__init__()
-        self.X  = set([x for x in range(0, 12)])
-        self.Y1 = [0, 1, 2, 3, 4, 5, 6]
-        self.Y2 = [5, 6, 8, 9]
-        self.Y3 = [1, 4, 7, 10]
-        self.Y4 = [2, 5, 8, 11]
-        self.Y5 = [3, 6, 9, 12]
-        self.Y6 = [10, 11]
+        self.X = []
+        self.F = []
+        self.data_generator(100)
+        
+    def data_generator(self, N):
+        self.X = range(N)
+        self.S0 = random.sample(self.X, 20)
+        
+        U = set(self.S0)
+        F = [self.S0]
+        i = 1
+        while len(set(self.X) - set(U)) > 20:
+            F.append([])
+            n = random.randint(1, 20)
+            x = random.randint(1, n)
+            for j in range(0, i-1):
+                U = U | set(F[j])
+            
+            remind_items = set(self.X) - U
+            F[i].extend(random.sample(remind_items, x))
+            F[i].extend(random.sample(U, n-x))
+            i += 1
+        F.append([])
+        remind = list(set(self.X) - U)
+        F[i].extend(remind)
+        
+        
+        cur_size = len(F)
+        remind_size = len(self.X) - cur_size
+        
+        for item in range(0, remind_size):
+            n = random.randint(0, int(N/2))
+            sample = random.sample(self.X, n)
+            F.append(sample)
+        
+        self.F = F
+        self.X = set(self.X)
         
     def greedy(self):
-        F = [self.Y1, self.Y2, self.Y3, self.Y4, self.Y5, self.Y6]
+        F = self.F
         U = self.X
         C = []
         state = [0]*len(F)
@@ -39,54 +70,13 @@ class SCP(object):
     
     def LP(self):
         C = []
-        F = [self.Y1, self.Y2, self.Y3, self.Y4, self.Y5, self.Y6]
-        sub_sets = []
-        for i in range(0, len(F)):
-            sub_sets.append('Sub{}'.format(i+1))
-        
-        subject = []
-        for i in range(0, 12):
-            st = [-1]*len(F)
-            for j in range(0, len(F)):
-                if i in F[j]:
-                    st[j] = 1
-                else:
-                    st[j] = 0
-            subject.append(st)
-        
-        scp_model = pulp.LpProblem('SCP', pulp.LpMinimize)
-        x = pulp.LpVariable.dict('x_%s', sub_sets, lowBound=0, upBound=1)
-        cost = dict(zip(sub_sets, [1]*len(F)))
-        
-        scp_model += sum( cost[i] * x[i] for i in sub_sets)
-        parameters = []
-        for i in range(0, len(subject)):
-            pars = dict(zip(sub_sets, subject[i]))
-            parameters.append(pars)
-        
-        for item in range(0, len(parameters)):
-            scp_model += sum( [parameters[item][i]*x[i] for i in sub_sets]) >= 1.0
-            
-        scp_model.solve()
-        lp_result = [x[item].value() for item in sub_sets]
-        f = max([sum(item) for item in subject])
-        if f == 0:
-            f = 0.0001
-            
-        for i in range(0, len(lp_result)):
-            if lp_result[i] > 1.0 / f:
-                C.append(F[i])
-        return C
-
-    def round(self):
-        C = []
-        F = [self.Y1, self.Y2, self.Y3, self.Y4, self.Y5, self.Y6]
+        F = self.F
         sub_sets = []
         for i in range(0, len(F)):
             sub_sets.append('Sub{}'.format(i + 1))
-
+        
         subject = []
-        for i in range(0, 12):
+        for i in range(0, len(self.X)):
             st = [-1] * len(F)
             for j in range(0, len(F)):
                 if i in F[j]:
@@ -94,7 +84,7 @@ class SCP(object):
                 else:
                     st[j] = 0
             subject.append(st)
-    
+
         scp_model = pulp.LpProblem('SCP', pulp.LpMinimize)
         x = pulp.LpVariable.dict('x_%s', sub_sets, lowBound=0, upBound=1)
         cost = dict(zip(sub_sets, [1] * len(F)))
@@ -112,19 +102,21 @@ class SCP(object):
         scp_model.solve()
     
         lp_result = [x[item].value() for item in sub_sets]
+        print(lp_result)
         f = max([sum(item) for item in subject])
+        print(f)
         if f == 0:
             f = 0.0001
     
         for i in range(0, len(lp_result)):
-            if lp_result[i] > 1.0 / 2:
+            if lp_result[i] > 1.0 / f:
                 C.append(F[i])
     
         return C
+    
+    
 if __name__ == '__main__':
     S = SCP()
-    #print(S.greedy())
-    LP = S.LP()
-    ROUND = S.round()
-    assert LP == ROUND
-    
+    g = S.greedy()
+    ROUND = S.LP()
+    print (ROUND)
