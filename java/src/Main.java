b236a49851708c0a85b0ee7d54b8eadd1a699560
MYHITGITBIT <1120310228@hit.edu.cn>
2017-08-03 14:18:47 +0800

    Add the implemnets of select sort

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,17 +1,26 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.print(less(10, 9));
+        int [] data = {7, 3, 5, 8, 2, 6, 4, 1, 0};
+        SelectSort(data);
+        assert isSort(data);
+        show(data);
+
     }
 
     public static boolean less(int a, int b) {
         return  a < b;
     }
 
+    public static void exch(int[] numArray, int i, int j) {
+        int temp = numArray[i];
+        numArray[i] = numArray[j];
+        numArray[j] = temp;
+    }
+
     public static void show(int [] numArray) {
         for (int i = 0; i < numArray.length; i++)
-            System.out.printf("The %d th element is %d", i, numArray[i]);
+            System.out.printf("The %d th element is %d\n", i, numArray[i]);
     }
 
     public static boolean  isSort(int [] numArray) {
@@ -20,6 +29,7 @@ public class Main {
                 return false;
             }
         }
+        return true;
     }
 
     public static void BubbleSort(int[] numArray) {
@@ -27,6 +37,12 @@ public class Main {
     }
 
     public static void SelectSort(int[] numArray) {
-
+        for (int i = 0; i < numArray.length; i++){
+            int min = i;
+            for (int j = i + 1; j < numArray.length; j++) {
+                if (less(numArray[j], numArray[min])) min = j;
+            }
+            exch(numArray, i, min);
+        }
     }
 }
