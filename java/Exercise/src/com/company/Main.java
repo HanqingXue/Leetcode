99001f78a2cec4424ac3060fc14e5c779b893f82
MYHITGITBIT <1120310228@hit.edu.cn>
2017-08-03 11:28:43 +0800

    An algorithm to find neareast integer of lg(n)

---
+++ java/Exercise/src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+import java.lang.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int v = lg(17);
+        System.out.println(v);
+
+    }
+    //Page 34
+    public static int lg(int N) {
+        sum++;
+    }
+    int log = 0;
+        for (int i = 0; i < sum; i++)
+    log += i;
+        return log;
+}
+}
+
+        int sum  =  0;
+        while (N / 2 != 0) {
+            N = N / 2;
